23aac3e1eb4f217ce61bfbacc7aa6704848087f0
Necdet Can Ateşman <can.atesman@strg.at>
2017-12-20 16:43:04 +0100

    Release: 0.0.2

--- score/sa/db/__init__.py
+++ score/sa/db/__init__.py
@@ -27,7 +27,7 @@
 from ._init import init, ConfiguredSaDbModule, engine_from_config
 from ._enum import Enum, EnumType
 
-__version__ = '0.0.1'
+__version__ = '0.0.2'
 
 __all__ = (
     'init', 'ConfiguredSaDbModule', 'engine_from_config', 'Enum', 'EnumType')

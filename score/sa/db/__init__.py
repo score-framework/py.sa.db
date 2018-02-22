d1011c162ec94fd0693e7c9723a8d8a5b0734515
Necdet Can Ateşman <can@atesman.at>
2018-02-22 16:16:41 +0100

    Release: 0.0.3

--- score/sa/db/__init__.py
+++ score/sa/db/__init__.py
@@ -27,7 +27,7 @@
 from ._init import init, ConfiguredSaDbModule, engine_from_config
 from ._enum import Enum, EnumType
 
-__version__ = '0.0.2'
+__version__ = '0.0.3'
 
 __all__ = (
     'init', 'ConfiguredSaDbModule', 'engine_from_config', 'Enum', 'EnumType')
--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ with open(os.path.join(here, 'README.rst')) as f:
 
 setup(
     name='score.sa.db',
-    version='0.0.2',
+    version='0.0.3',
     description='Database connectivity for The SCORE Framework',
     long_description=README,
     author='strg.at',
